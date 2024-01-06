48636bd718daf6b876b2817320a0e9136e552ab6
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-01-06 12:21:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6573749352:AAFH7npSgDPg7euudqgAMoANLxapk83AUWM",
-             api_id= 10499690,
+             bot_token= "6600128329:AAH56-MVyQUSQiC-CgORcy0tO-uOPzcq2g4",
+             api_id=10499690,
              api_hash= "87d0414dc159c10225cac921edde640a")
 auth_users = [6585878012,-4094035957]
 
