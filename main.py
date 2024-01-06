df53569501e72001afddcfd87189afe232e8d313
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-01-06 12:48:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ bot = Client("bot",
              bot_token= "6600128329:AAH56-MVyQUSQiC-CgORcy0tO-uOPzcq2g4",
              api_id=10499690,
              api_hash= "87d0414dc159c10225cac921edde640a")
-auth_users = [6585878012,-4094035957]
+auth_users = [6585878012,-1002122540539]
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
